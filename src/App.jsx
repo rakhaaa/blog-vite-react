595d2d332067031047a1156d1e09b61c7445b266
rakhaaa <51009780+rakhaaa@users.noreply.github.com>
2024-02-25 05:32:54 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Learn to build blog website
+        Learn React For Better Future
       </p>
     </>
   )
